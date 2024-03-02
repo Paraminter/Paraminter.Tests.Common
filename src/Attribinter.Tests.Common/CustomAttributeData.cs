db0c677b58f258f9a3c00a6b6fd7e35a5e90819b
ErikWe <erikwestberg7@gmail.com>
2024-03-02 16:53:09 +0100

    Added CustomAttributeData

---
+++ src/Attribinter.Tests.Common/CustomAttributeData.cs
@@ -0,0 +1,25 @@
+﻿namespace Attribinter;
+
+using Microsoft.CodeAnalysis;
+
+using System.Collections.Generic;
+using System.Collections.Immutable;
+
+internal sealed class CustomAttributeData : AttributeData
+{
+    new public INamedTypeSymbol? AttributeClass { get; init; }
+    new public IMethodSymbol? AttributeConstructor { get; init; }
+
+    new public ImmutableArray<TypedConstant> ConstructorArguments { get; init; }
+    new public ImmutableArray<KeyValuePair<string, TypedConstant>> NamedArguments { get; init; }
+
+    new public SyntaxReference? ApplicationSyntaxReference { get; init; }
+
+    protected override INamedTypeSymbol? CommonAttributeClass => AttributeClass;
+    protected override IMethodSymbol? CommonAttributeConstructor => AttributeConstructor;
+
+    protected override ImmutableArray<TypedConstant> CommonConstructorArguments => ConstructorArguments;
+    protected override ImmutableArray<KeyValuePair<string, TypedConstant>> CommonNamedArguments => NamedArguments;
+
+    protected override SyntaxReference? CommonApplicationSyntaxReference => ApplicationSyntaxReference;
+}
